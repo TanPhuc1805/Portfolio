77be4305c8a662e2d945466fdd24a54cb2952854
Võ Chánh Tín <tin.vo.4767@outlook.com>
2024-07-10 23:16:55 +0700

    Rebase the branch from Dan Sam's commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,104 +1,44 @@
 import { Model } from "./components/Model/Scene";
 import { Avatar } from "./components/Model/Avatar";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { OrbitControls, Scroll, ScrollControls } from "@react-three/drei";
 import { motion } from "framer-motion-3d";
 import React from "react";
 import "./App.css";
-import "./components/NavigationBar/NavigationBar";
+import "./components/NavigationBar/NavigationBar";;
 import NavigationBar from "./components/NavigationBar/NavigationBar";
+import { Interface } from "./components/ScrollItem/Interface";
 
-function About(props) {
-  const { setSection } = props;
-  return (
-    <div className="absolute top-0 left-5 p-8 text-white h-full flex flex-col justify-center">
-      <h1 className="text-4xl md:text-6xl font-[K2D] leading-snug mt-8 md:mt-0">
-        Hello, We're
-        <br />
-        <span className="px-1 font-[K2D]"> HeyDucks Team</span>
-      </h1>
-      <motion.p
-        className="text-lg text-gray-200 mt-4 font-[K2D]"
-        initial={{
-          opacity: 0,
-          y: 25,
-        }}
-        whileInView={{
-          opacity: 1,
-          y: 0,
-        }}
-        transition={{
-          duration: 1,
-          delay: 1.5,
-        }}
-      >
-        We specialize in creating 3D visuals,
-        <br />
-        designing user interfaces, and developing web applications.
-      </motion.p>
-      <motion.button
-        onClick={() => setSection(3)}
-        className="bg-indigo-600 text-white py-4 px-8 rounded-lg font-[K2D] text-lg mt-4 md:mt-16"
-        initial={{
-          opacity: 0,
-          y: 25,
-        }}
-        whileInView={{
-          opacity: 1,
-          y: 0,
-        }}
-        transition={{
-          duration: 1,
-          delay: 2,
-        }}
-      >
-        Contact us now
-      </motion.button>
-    </div>
-  );
-}
 function Model3D() {
   return (
-    <Canvas style={{ height: "100%" }}>
-      {/* <color attach="background" args={["#E87339"]} /> */}
-      <ambientLight intensity={0.5} />
-      <directionalLight position={[5, 5, 5]} intensity={3} />
-      <motion.group
-        rotation={[0.4, -Math.PI / 3, 0.1]}
-        scale={0.4}
-        position={[2.25, -0.5, 0]}
-      >
-        <Model />
-      </motion.group>
-      <OrbitControls minDistance={2} maxDistance={10} />
-    </Canvas>
-  );
-}
-
-function Avatar3D() {
-  return (
-    <Canvas style={{ height: "100%" }}>
-      {/* <color attach="background" args={["#E87339"]} /> */}
-      <ambientLight intensity={1} />
-      <directionalLight position={[5, 5, 5]} intensity={3} />
-      <motion.group
-        rotation={[0.3, -Math.PI / 5, 0.1]}
-        scale={2}
-        position={[2, -2, 1]}
-      >
-        <Avatar />
-      </motion.group>
-      <OrbitControls minDistance={2} maxDistance={10} />
-    </Canvas>
+    <>
+      <Canvas style={{ height: "100%" }}>
+        <ambientLight intensity={0.5} />
+        <directionalLight position={[5, 5, 5]} intensity={10} />
+        <ScrollControls pages={7} damping={0.1}>
+          <motion.group
+            rotation={[0.4, -Math.PI / 3, 0.1]}
+            scale={0.4}
+            position={[1.25, -0.5, 0]}
+          >
+            <Model />
+          </motion.group>
+          <Scroll html>
+            {/* <About /> */}
+            <Interface />
+          </Scroll>
+        </ScrollControls>
+        {/* <OrbitControls minDistance={2} maxDistance={10} /> */}
+      </Canvas>
+    </>
   );
 }
 
 function App() {
   return (
     <div className="relative h-screen">
-      <About />
-      {/* <Model3D /> */}
-      <Avatar3D />
+      {/* <About /> */}
+      <Model3D />
       <NavigationBar />
       <div className="gradient-background absolute inset-0 z-[-1]">
         <div className="gradients-container">
---
+++ src/components/ScrollItem/Interface.jsx
@@ -0,0 +1,144 @@
+import React from "react";
+
+const skills = [
+  {
+    title: "ThreeJS / React Three Fiber",
+    level: 80,
+  },
+  {
+    title: "ReactJS",
+    level: 95,
+  },
+  {
+    title: "Angular",
+    level: 60,
+  },
+  {
+    title: "NodeJS",
+    level: 75,
+  },
+  {
+    title: "Android/iOS developement",
+    level: 80,
+  },
+  {
+    title: "Python",
+    level: 70,
+  },
+];
+
+const languages = [
+  {
+    title: "🇻🇳 Vietnamese",
+    level: 100,
+  },
+  {
+    title: "🏴󠁧󠁢󠁥󠁮󠁧󠁿 English",
+    level: 80,
+  },
+  {
+    title: "🇩🇪 Deutsch",
+    level: 70,
+  },
+];
+
+const Section = (props) => {
+  const { children } = props;
+
+  return (
+    <section className="h-screen w-screen p-8 max-w-screen-2xl mx-auto flex flex-col items-start justify-center">
+      {children}
+    </section>
+  );
+};
+
+const AboutSection = () => {
+  return (
+    <>
+      <Section>
+        <h1 className="text-4xl md:text-6xl font-[K2D] leading-snug mt-8 md:mt-0 text-gray-200">
+          Hi, We're
+          <br />
+          <span className="px-1 font-[K2D]">HeyDucks Team</span>
+        </h1>
+        <p className="text-lg text-gray-200 mt-4 font-[K2D]">
+          We specialize in creating 3D visuals,
+          <br />
+          designing user interfaces, and developing web applications.
+        </p>
+        <button
+          className={`bg-indigo-600 text-white py-4 px-8 rounded-lg font-bold text-lg mt-16`}
+        >
+          Contact us
+        </button>
+      </Section>
+    </>
+  );
+};
+
+const SkillSection = () => {
+  return (
+    <Section>
+      <div>
+        <h2 className="text-5xl font-bold font-[K2D] text-gray-200">Skills</h2>
+        <div className="mt-8 space-y-4">
+          {skills.map((skill, index) => {
+            return (
+              <div className="w-64" key={index}>
+                <h3 className="text-xl font-bold text-gray-200 font-[K2D]">
+                  {skill.title}
+                </h3>
+                <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                  <div
+                    className="h-full bg-indigo-500 rounded-full"
+                    style={{ width: `${skill.level}%` }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+
+        <h2 className="text-5xl font-bold font-[K2D] mt-20 text-gray-200">Languages</h2>
+        <div className="mt-8 space-y-4">
+          {languages.map((skill, index) => {
+            return (
+              <div className="w-64" key={index}>
+                <h3 className="text-xl font-bold text-gray-200 font-[K2D]">
+                  {skill.title}
+                </h3>
+                <div className="h-2 w-full bg-gray-200 rounded-full mt-2">
+                  <div
+                    className="h-full bg-indigo-500 rounded-full"
+                    style={{ width: `${skill.level}%` }}
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </Section>
+  );
+};
+
+export const Interface = () => {
+  return (
+    <>
+      <div className="flex flex-col items-center w-screen">
+        <Section>
+          <AboutSection />
+        </Section>
+        <Section>
+          <SkillSection />
+        </Section>
+        <Section>
+          <h1>Projects</h1>
+        </Section>
+        <Section>
+          <h1>Contact</h1>
+        </Section>
+      </div>
+    </>
+  );
+};
