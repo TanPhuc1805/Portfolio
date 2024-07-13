45fc38c3dc40e05763cfa15a54e0d209a3b44b7e
Võ Chánh Tín <tin.vo.4767@outlook.com>
2024-07-13 14:59:37 +0700

    beautify contact section

--- src/components/ScrollItem/Interface.jsx
+++ src/components/ScrollItem/Interface.jsx
@@ -1,4 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import Input from "@mui/joy/Input";
+import Textarea from "@mui/joy/Textarea";
+import { Button, FormControl, FormHelperText } from "@mui/joy";
+import Send from "@mui/icons-material/Send";
+import InfoOutlined from "@mui/icons-material/InfoOutlined";
+import PrivacyTipIcon from "@mui/icons-material/PrivacyTip";
+import PersonIcon from "@mui/icons-material/Person";
+import AlternateEmailIcon from "@mui/icons-material/AlternateEmail";
+import PhoneIcon from "@mui/icons-material/Phone";
+import Snackbar from '@mui/joy/Snackbar';
+import NorthWestIcon from '@mui/icons-material/NorthWest';
+import NorthEastIcon from '@mui/icons-material/NorthEast';
+import NorthIcon from '@mui/icons-material/North';
+import SouthIcon from '@mui/icons-material/South';
+import SouthEastIcon from '@mui/icons-material/SouthEast';
+import SouthWestIcon from '@mui/icons-material/SouthWest';
+
+const validateEmail = (email) => {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email);
+};
 
 const skills = [
   {
@@ -125,68 +146,150 @@ const SkillSection = () => {
 };
 
 const ContactSession = () => {
-  return [
-    <Section>
-      <h2 className="text-5xl font-bold">Contact me</h2>
-      <div className="mt-8 p-8 rounded-md bg-white w-96 max-w-full">
-        <form>
-          <label for="name" className="font-medium text-gray-900 block mb-1">
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm"
-            placeholder="Enter your name here..."
-          />
+  const [isSendButtonLoading, setIsSendButtonLoading] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
-          <label
-            for="email"
-            className="font-medium text-gray-900 block mb-1 mt-5"
-          >
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm"
-            placeholder="Enter your email here..."
-          />
+  return (
+    <>
+      <Section>
+        <h2 className="text-5xl md:text-6xl font-[K2D] leading-snug mt-8 md:mt-0 text-gray-200">
+          Contact us!
+        </h2>
+        <div className="mt-8 p-8 rounded-md bg-white w-5/12 max-w-full">
+          <form>
+            <FormControl
+              error={name !== null && name.length === 0 ? true : false}
+            >
+              <label
+                htmlFor="name"
+                className="font-medium text-gray-900 block mb-1"
+              >
+                Name
+              </label>
+              <Input
+                onChange={(e) => setName(e.target.value)}
+                type="text"
+                name="name"
+                id="name"
+                startDecorator={<PersonIcon />}
+                placeholder="Enter your name here..."
+                required
+              />
+              {name !== null && name.length === 0 ? (
+                <FormHelperText>
+                  <InfoOutlined />
+                  Your name is required!
+                </FormHelperText>
+              ) : (
+                ""
+              )}
+            </FormControl>
 
-          <label
-            for="phoneNumber"
-            className="font-medium text-gray-900 block mb-1 mt-5"
-          >
-            Phone number
-          </label>
-          <input
-            type="tel"
-            name="phoneNumber"
-            id="phoneNumber"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm"
-            placeholder="Enter your phone number here..."
-          />
+            <FormControl
+              error={
+                email !== null && (email.length === 0 || !validateEmail(email))
+                  ? true
+                  : false
+              }
+            >
+              <label
+                htmlFor="email"
+                className="font-medium text-gray-900 block mb-1 mt-5"
+              >
+                Email
+              </label>
+              <Input
+                onChange={(e) => setEmail(e.target.value)}
+                type="email"
+                name="email"
+                id="email"
+                startDecorator={<AlternateEmailIcon />}
+                placeholder="Enter your email here..."
+                required
+              />
+              {email !== null && email.length === 0 ? (
+                <FormHelperText>
+                  <InfoOutlined />
+                  Your email is required!
+                </FormHelperText>
+              ) : (
+                ""
+              )}
+              {email !== null && email.length > 0 && !validateEmail(email) ? (
+                <FormHelperText>
+                  <InfoOutlined />
+                  Your email is invalid!
+                </FormHelperText>
+              ) : (
+                ""
+              )}
+            </FormControl>
 
-          <label
-            for="message"
-            className="font-medium text-gray-900 block mb-1 mt-5"
-          >
-            Message
-          </label>
-          <textarea
-            name="message"
-            id="message"
-            className="block w-full rounded-md border-0 text-gray-900 shadow-sm"
-            placeholder="Enter your message here..."
-          />
+            <FormControl>
+              <label
+                htmlFor="phoneNumber"
+                className="font-medium text-gray-900 block mb-1 mt-5"
+              >
+                Phone number
+              </label>
+              <Input
+                type="tel"
+                name="phoneNumber"
+                id="phoneNumber"
+                startDecorator={<PhoneIcon />}
+                placeholder="Enter your phone number here..."
+              />
+            </FormControl>
 
-          <input type="submit" value="Send" className="w-full bg-blue-500 text-white py-2 px-8 rounded-lg font-bold text-lg mt-5"/>
-        </form>
-      </div>
-    </Section>,
-  ];
+            <FormControl
+              error={message !== null && message.length === 0 ? true : false}
+            >
+              <label
+                htmlFor="message"
+                className="font-medium text-gray-900 block mb-1 mt-5"
+              >
+                Message
+              </label>
+              <Textarea
+                name="message"
+                id="message"
+                className="block w-full rounded-md border-0 text-gray-900 shadow-sm"
+                placeholder="Enter your message here..."
+                onChange={(e) => setMessage(e.target.value)}
+                required
+              />
+              {message !== null && message.length === 0 ? (
+                <FormHelperText>
+                  <InfoOutlined />
+                  Your message is required!
+                </FormHelperText>
+              ) : (
+                ""
+              )}
+            </FormControl>
+
+            <FormHelperText style={{ marginTop: "2rem" }}>
+              <PrivacyTipIcon />
+              We'll never share your email with anyone else.
+            </FormHelperText>
+
+            <Button
+              // type="submit"
+              className="w-full"
+              style={{ marginTop: "2rem" }}
+              loading={isSendButtonLoading}
+              onClick={() => setIsSendButtonLoading(true)}
+              startDecorator={<Send />}
+            >
+              Send
+            </Button>
+          </form>
+        </div>
+      </Section>
+    </>
+  );
 };
 
 export const Interface = () => {
